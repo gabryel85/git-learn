515d0188b5e490c0e443911d7167b43b06fa87a7
Paweł gabryel <pawel.gabryel@apps.clinic>
2024-02-06 16:49:33 +0100

    remove a

--- src/App.js
+++ src/App.js
@@ -5,9 +5,6 @@ function App() {
 	return (
 		<div className='App'>
 			<p>Hello World!</p>
-			<a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-				Test
-			</a>
 		</div>
 	);
 }
