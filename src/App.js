811570539bb69f605443b8843ea9f86f6847cd41
Paweł gabryel <pawel.gabryel@apps.clinic>
2024-02-06 16:44:57 +0100

    test

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
 		<div className='App'>
 			<p>Hello World!</p>
 			<a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-				Learn React
+				Test
 			</a>
 		</div>
 	);
